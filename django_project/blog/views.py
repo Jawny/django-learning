c1deb11242e5b7068b35e2ff8f61ed249682459b
Johnny Liao <johnnyliao06@gmail.com>
2023-10-13 02:03:22 -0700

    wrote templates and setup bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+        {
+            'author': 'johnny',
+            'title': 'Blog post 1',
+            'content': 'first post',
+            'date_posted': 'October 13, 2023'
+            },
+        {
+            'author': 'bob',
+            'title': 'Blog post 2',
+            'content': 'first post',
+            'date_posted': 'October 20, 2023'
+            },
+        {
+            'author': 'bill',
+            'title': 'Blog post 3',
+            'content': 'first post',
+            'date_posted': 'October 23, 2023'
+            },
+        ]
+
+def home(request):
+    context = {
+            'posts': posts
+            }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
