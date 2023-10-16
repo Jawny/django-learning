4fb5a40449bf574fb57cacc03ed05fff10aa1614
Johnny Liao <johnnyliao06@gmail.com>
2023-10-16 13:12:08 -0700

    finished corey schafer tutorial part 5

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,30 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts = [
-        {
-            'author': 'johnny',
-            'title': 'Blog post 1',
-            'content': 'first post',
-            'date_posted': 'October 13, 2023'
-            },
-        {
-            'author': 'bob',
-            'title': 'Blog post 2',
-            'content': 'first post',
-            'date_posted': 'October 20, 2023'
-            },
-        {
-            'author': 'bill',
-            'title': 'Blog post 3',
-            'content': 'first post',
-            'date_posted': 'October 23, 2023'
-            },
-        ]
+from .models import Post
 
 def home(request):
     context = {
-            'posts': posts
+            'posts': Post.objects.all() 
             }
     return render(request, 'blog/home.html', context)
 
